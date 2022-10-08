91c0797fc710fd4a1f3be6a089c225b8b78bbcb6
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-10-08 23:40:39 +0530

    made navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@
 
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ChannelDetail from "./components/ChannelDetail";
+import Feed from "./components/Feed";
+import Navbar from "./components/Navbar";
+import SearchFeed from "./components/SearchFeed";
+import VideoDetail from "./components/VideoDetail";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
-        <Route path="" element={<SearchFeed />} />
+        <Route path="/search" element={<SearchFeed />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
 /** @format */
 
 import React from "react";
+import { BsYoutube } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="sticky top-0 flex items-center justify-between h-16 p-2 bg-black">
+      <Link to="/" className="flex space-x-1">
+        <BsYoutube color="red" size={30} />
+        <h1 className="text-xl font-bold text-white">YouTube</h1>
+      </Link>
+      <SearchBar />
+    </div>
+  );
 };
 
 export default Navbar;
