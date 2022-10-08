e3dc141a7ae65e54b83dbd0fdc452dc329c869b4
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-10-08 23:01:33 +0530

    used react router for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 /** @format */
 
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="bg-red-500">hello tailwind</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="" element={<SearchFeed />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
