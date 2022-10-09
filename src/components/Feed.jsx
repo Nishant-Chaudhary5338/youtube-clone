c80c16a66d151fafadb9b5b2cdf2f878253f662f
Nishant-Chaudhary5338 <nishantchaudhary5338@gmail.com>
2022-10-09 17:18:44 +0530

    made some changed in SideBar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,33 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getVideos } from "../Api";
 import SideBar from "./SideBar";
+import VideoRow from "./VideoRow";
 
 const Feed = () => {
+  const [selectedCategory, setselectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const token = getVideos(`search?query=${selectedCategory}`);
+    token.then((v) => {
+      setVideos(v);
+      console.log(v);
+    });
+  }, [selectedCategory]);
+
   return (
-    <div className="h-screen bg-black border-t-2 border-gray-800">
-      <SideBar />
+    <div className="flex h-screen bg-black border-t-2 border-gray-800">
+      <SideBar
+        selectedCategory={selectedCategory}
+        setselectedCategory={setselectedCategory}
+      />
+      <div>
+        <h4 className="pl-2 text-2xl font-bold text-white">
+          {selectedCategory}
+        </h4>
+        <VideoRow videos={videos} />
+      </div>
     </div>
   );
 };
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,11 +3,16 @@
 import React from "react";
 import { videoCategories } from "../utils/constants";
 
-const SideBar = () => {
+const SideBar = ({ selectedCategory, setselectedCategory }) => {
   return (
     <div className="hidden w-40 border-r-2 border-gray-800 sm:block">
       {videoCategories.map((v) => (
-        <div className="px-4 py-2 space-x-4 hover:bg-gray-800">
+        <div
+          key={v.name}
+          onClick={() => setselectedCategory(v.name)}
+          style={{ background: v.name === selectedCategory && "#FC1503" }}
+          className="px-4 py-2 space-x-4 rounded-2xl hover:bg-gray-800"
+        >
           <button className="flex items-center space-x-4">
             <span>{v.icon}</span>
             <span className="text-sm font-semibold text-white">{v.name}</span>
