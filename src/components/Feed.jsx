710e193bc57bf657863ba780a0b9826f04c4a67c
Nishant Chaudhary <nishantchaudhary5338@gmail.com>
2022-12-24 12:19:07 +0530

    made sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,26 @@
 /** @format */
 
+import { Box, Stack } from "@mui/material";
 import React from "react";
+import SideBar from "./SideBar";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "92vh", flex: 2 }}>
+        <h4 className="font-semibold text-white">Videos</h4>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import { Category } from "@mui/icons-material";
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { categories } from "../utils/constants";
+const selectedCategory = "New";
+const SideBar = () => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((c) => (
+        <button
+          style={{ background: c.name === selectedCategory && "#FC1503" }}
+          className="text-white space-x-4 my-1 mx-4  flex items-center rounded-2xl p-2 hover:bg-[#FC1503]"
+          key={c.name}
+        >
+          <span
+            className=""
+            style={{ color: c.name === selectedCategory ? "white" : "red" }}
+          >
+            {c.icon}
+          </span>
+          <span style={{ opacity: c.name === selectedCategory ? "1" : "0.8" }}>
+            {c.name}
+          </span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default SideBar;
