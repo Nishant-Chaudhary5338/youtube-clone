5aa4f6aa17afbe165cc83619b9a167d200f09d8c
Nishant Chaudhary <nishantchaudhary5338@gmail.com>
2023-01-28 01:40:06 +0530

    made channel page

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -6,30 +6,32 @@ import { demoProfilePicture } from "../utils/constants";
 
 const ChannelCard = ({ channelDetail }) => {
   return (
-    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-      <div className="pb-4 m-2 space-y-2">
-        <div className="w-60">
-          <img
-            className="w-32 mx-auto rounded-full"
-            src={
-              channelDetail?.snippet?.thumbnails?.high?.url ||
-              demoProfilePicture
-            }
-            alt={channelDetail?.snippet?.title}
-          />
-        </div>
-        <div>
-          <h4 className="text-sm font-bold text-center text-white">
-            {channelDetail?.snippet?.title}
-          </h4>
-          <div className="space-x-2 text-xs font-semibold text-center text-gray-500">
-            <span>{channelDetail?.statistics?.subscriberCount}</span>
-            <span>.</span>
-            <span>{channelDetail?.statistics?.videoCount} videos</span>
+    <div>
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <div className="pb-4 m-2 space-y-2">
+          <div className="w-60">
+            <img
+              className="w-32 mx-auto rounded-full"
+              src={
+                channelDetail?.snippet?.thumbnails?.high?.url ||
+                demoProfilePicture
+              }
+              alt={channelDetail?.snippet?.title}
+            />
+          </div>
+          <div>
+            <h4 className="text-sm font-bold text-center text-white">
+              {channelDetail?.snippet?.title}
+            </h4>
+            <div className="space-x-2 text-xs font-semibold text-center text-gray-500">
+              <span>{channelDetail?.statistics?.subscriberCount}</span>
+              <span>.</span>
+              <span>{channelDetail?.statistics?.videoCount} videos</span>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -5,29 +5,36 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchFromAPI, getVideos } from "../utils/Api";
 import ChannelCard from "./ChannelCard";
+import VideoRow from "./VideoRow";
 
 const ChannelDetail = () => {
   const [ChannelDetail, setChannelDetail] = useState(null);
   const [videos, setVideos] = useState([]);
   const { id } = useParams();
   useEffect(() => {
-    const token = getVideos(`channels?part=snippet&id=${id}`);
+    const token = fetchFromAPI(`channels?part=snippet&id=${id}`);
     token.then((v) => {
-      setChannelDetail(v);
-      console.log(v);
+      setChannelDetail(v?.items[0]);
+      console.log(v?.items[0]);
     });
     const video = fetchFromAPI(
       `search?channelID=${id}&part=snippet&order=date`
     );
     video.then((v) => {
-      setVideos(v);
-      console.log(v);
+      setVideos(v?.items);
+      console.log(v?.items);
     });
   }, [id]);
   return (
     <Box minHeight="95vh">
       <Box>
-        <div className="z-10 bg-yellow-500 h-72"></div>
+        <div className="z-10 bg-yellow-500 h-72">
+          <ChannelCard channelDetail={ChannelDetail} />
+        </div>
+      </Box>
+      <Box display="flex" p={2}>
+        <Box sx={{ mr: { sm: "100px" } }}></Box>
+        <VideoRow videos={videos} />
       </Box>
     </Box>
   );
